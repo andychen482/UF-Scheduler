0dccf234ef356fff095fc609119fcbf088a9dcc0
andychen482 <andy.chen0607@gmail.com>
2023-07-26 18:01:29 -0400

    GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Main from "./pages/Main/Main";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="WAS" element={<Main />} />
     </Routes>
   );
 }
