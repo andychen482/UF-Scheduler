a9342561fe798d9de9d0874014189530c37976e3
andychen482 <andy.chen0607@gmail.com>
2023-08-02 15:49:07 -0400

    Removed horizontal scrolling

--- src/components/CoursesHandler/LikedSelectedCourses.tsx
+++ src/components/CoursesHandler/LikedSelectedCourses.tsx
@@ -41,29 +41,43 @@ const LikedSelectedCourses: React.FC<LikedSelectedCoursesProps> = ({
     setLoaded(true);
   };
 
+  // Function to chunk the selected courses into pairs
+  const chunkArray = (array: Course[], chunkSize: number) => {
+    const chunkedArray = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      chunkedArray.push(array.slice(i, i + chunkSize));
+    }
+    return chunkedArray;
+  };
+
+  const selectedCoursesChunks = chunkArray(selectedCourses, 2);
+
   return (
     <>
-      <div className="space-x-2 mb-4 flex flex-wrap">
-        {selectedCourses.length > 0 &&
-          selectedCourses.map((course: Course, index: number) => (
-            <div
-              key={index}
-              className={`p-4 rounded-md m-2 text-black dark:text-white cursor-pointer sm:w-[18.5rem] w-full h-20 overflow-hidden`}
-              style={getCourseBackgroundColor(course)}
-              onClick={() => handleBadgeClick(course)}
-            >
-              {course.termInd !== " " && course.termInd !== "C" ? (
-                <strong>
-                  {course.code.replace(/([A-Z]+)/g, "$1 ")} - {course.termInd}
-                </strong>
-              ) : (
-                <strong>{course.code.replace(/([A-Z]+)/g, "$1 ")}</strong>
-              )}
-              <div className="text-sm line-clamp-1 overflow-ellipsis overflow-hidden">
-                {course.name}
+      <div className="mb-4">
+        {selectedCoursesChunks.map((courseChunk: Course[], chunkIndex: number) => (
+          <div key={chunkIndex} className="flex mb-4">
+            {courseChunk.map((course: Course, index: number) => (
+              <div
+                key={index}
+                className={`flex-1 p-4 rounded-md m-2 text-black dark:text-white cursor-pointer sm:w-[18.5rem] w-full h-20 overflow-hidden`}
+                style={getCourseBackgroundColor(course)}
+                onClick={() => handleBadgeClick(course)}
+              >
+                {course.termInd !== " " && course.termInd !== "C" ? (
+                  <strong>
+                    {course.code.replace(/([A-Z]+)/g, "$1 ")} - {course.termInd}
+                  </strong>
+                ) : (
+                  <strong>{course.code.replace(/([A-Z]+)/g, "$1 ")}</strong>
+                )}
+                <div className="text-sm line-clamp-1 overflow-ellipsis overflow-hidden">
+                  {course.name}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+        ))}
       </div>
     </>
   );
