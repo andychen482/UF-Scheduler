b09207e700941ad1f0b6f32b55926f588a74c890
andychen482 <andy.chen0607@gmail.com>
2023-08-04 15:04:21 -0400

    Fixed bug in major select

--- src/components/CoursesHandler/MajorSearch/MajorSearch.tsx
+++ src/components/CoursesHandler/MajorSearch/MajorSearch.tsx
@@ -18,8 +18,18 @@ const MajorSelect: React.FC<MajorSearchProps> = ({ selectedMajor, setSelectedMaj
     <div>
       <Select
         options={options}
+        isClearable={true}
         value={selectedMajor ? { value: selectedMajor, label: selectedMajor } : null}
         onChange={handleChange}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 0,
+          colors: {
+            ...theme.colors,
+            primary25: '#E6E6E6',
+            primary: '#B3B3B3',
+          },
+        })}
         placeholder="Select a department..."
         className="mb-4 text-black bg-gray-200 rounded-md placeholder-gray-500 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 transition-colors duration-500 w-[20rem] sm:w-[20rem] md:w-[24rem] lg:w-[28rem] xl:w-[32rem]"
       />
