b4068f75be496b261228ebbbf4bf25505094951b
andychen482 <andy.chen0607@gmail.com>
2023-09-26 09:44:58 -0400

    Maybe browserrouter?

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import "./App.css";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main/Main";
 import AboutPage from "./pages/About/About";
 
 function App() {
   return (
-    <Router>
       <Routes>
-      <Route path="" element={<Main />} />
-      <Route path="/about" element={<AboutPage />} />
+        <Route path="" element={<Main />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
-    </Router>
   );
 }
 
