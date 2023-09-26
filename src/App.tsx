6897028a3a977aaa51f41dafaafb27e6ad19e389
andychen482 <andy.chen0607@gmail.com>
2023-09-26 10:25:59 -0400

    Migration to vercel, back to browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import "./App.css";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Main from "./pages/Main/Main";
 import AboutPage from "./pages/About/About";
 
 function App() {
   return (
-    <Router>
-      <Routes>
+    <Routes>
       <Route path="" element={<Main />} />
       <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </Router>
+    </Routes>
   );
 }
 
