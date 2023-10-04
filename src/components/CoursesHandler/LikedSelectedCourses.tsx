2652c1e69c9f82267894dde93901e816fc5cea29
andychen482 <andy.chen0607@gmail.com>
2023-10-04 19:34:24 -0400

    added class number to badges

--- src/components/CoursesHandler/LikedSelectedCourses.tsx
+++ src/components/CoursesHandler/LikedSelectedCourses.tsx
@@ -21,6 +21,10 @@ const getHashedColor = (course: Course) => {
   return colorHash.hex(course.code + course.name);
 };
 
+const getSelectedSection = (course: Course): Section | undefined => {
+  return course.sections.find((section: Section) => section.selected === true);
+};
+
 const LikedSelectedCourses: React.FC<LikedSelectedCoursesProps> = ({
   selectedCourses,
   setSelectedCourses,
@@ -53,15 +57,11 @@ const LikedSelectedCourses: React.FC<LikedSelectedCoursesProps> = ({
   const handleAppointmentBadgeClick = (section: any) => {
     setCustomAppointments((prevAppointments) =>
       prevAppointments.filter(
-        (selectedAppointment) =>
-          !(
-            selectedAppointment === section
-          )
+        (selectedAppointment) => !(selectedAppointment === section)
       )
     );
     setLoaded(true);
   };
-  
 
   // Function to chunk the selected courses into pairs
   const chunkArray = (array: Course[], chunkSize: number) => {
@@ -97,39 +97,46 @@ const LikedSelectedCourses: React.FC<LikedSelectedCoursesProps> = ({
           selectedCoursesChunks.map(
             (courseChunk: Course[], chunkIndex: number) => (
               <div key={chunkIndex} className="flex mx-3">
-                {courseChunk.map((course: Course, index: number) => (
-                  <div
-                    id="badge"
-                    key={index}
-                    className={`flex-1 p-[0.6rem] rounded-md mb-2 text-black dark:text-white cursor-pointer w-full h-full overflow-hidden`}
-                    style={getCourseBackgroundColor(course)}
-                    onClick={() => handleBadgeClick(course)}
-                  >
-                    {/* Display course code and credits */}
-                    <div className="flex justify-between">
-                      <div className="flex-1 min-w-0">
-                        <div className="flex justify-between">
-                          {course.termInd !== " " && course.termInd !== "C" ? (
-                            <strong className="block truncate">
-                              {course.code.replace(/([A-Z]+)/g, "$1 ")} -{" "}
-                              {course.termInd}
-                            </strong>
-                          ) : (
-                            <strong className="block truncate">
-                              {course.code.replace(/([A-Z]+)/g, "$1 ")}
-                            </strong>
-                          )}
-                          <span className="text-white mt-[0.12rem] font-bold text-sm">
-                            {course.sections[0].credits}
-                          </span>
-                        </div>
-                        <div className="text-sm text-ellipsis">
-                          {course.name}
+                {courseChunk.map((course: Course, index: number) => {
+                  const selectedSection = getSelectedSection(course);
+                  return (
+                    <div
+                      id="badge"
+                      key={index}
+                      className={`flex-1 p-[0.6rem] rounded-md mb-2 text-black dark:text-white cursor-pointer w-full h-full overflow-hidden`}
+                      style={getCourseBackgroundColor(course)}
+                      onClick={() => handleBadgeClick(course)}
+                    >
+                      {/* Display course code, credits, and class number */}
+                      <div className="flex justify-between">
+                        <div className="flex-1 min-w-0">
+                          <div className="flex justify-between">
+                            {course.termInd !== " " &&
+                            course.termInd !== "C" ? (
+                              <strong className="block truncate">
+                                {course.code.replace(/([A-Z]+)/g, "$1 ")} -{" "}
+                                {course.termInd}
+                              </strong>
+                            ) : (
+                              <strong className="block truncate">
+                                {course.code.replace(/([A-Z]+)/g, "$1 ")}
+                              </strong>
+                            )}
+                            <span className="text-white mt-[0.12rem] font-bold text-sm">
+                              {course.sections[0].credits}
+                            </span>
+                          </div>
+                          <div className="text-sm text-ellipsis">
+                            {course.name}{" "}
+                                {selectedSection
+                                  ? `| Class # ${selectedSection.classNumber}`
+                                  : ""}
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  );
+                })}
               </div>
             )
           )
