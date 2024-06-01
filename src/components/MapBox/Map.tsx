714f538ea986f36fbd57ec11a90bcd9786065897
andychen482 <andy.chen0607@gmail.com>
2024-06-01 19:09:42 -0400

    parking map

--- src/components/MapBox/Map.tsx
+++ src/components/MapBox/Map.tsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import rawCoords from "../../data/buildingCoords.json";
+import parkingInfo from "../../data/parking_polys.json";
 import "./MapStyles.css";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN as string;
@@ -53,106 +54,154 @@ const Map = () => {
         // cooperativeGestures: true,
       });
 
-      map.on('load', function () {
+      map.on("load", function () {
         if (map) {
-        map.addLayer({
-          'id': 'add-3d-buildings',
-          'source': 'composite',
-          'source-layer': 'building',
-          'filter': ['==', 'extrude', 'true'],
-          'type': 'fill-extrusion',
-          'minzoom': 14.1,
-          'paint': {
-            'fill-extrusion-color': '#aaa',
-            'fill-extrusion-height': [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              14.1, 0,
-              14.15, ["get", "height"]
-            ],
-            'fill-extrusion-base': [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              14.1, 0,
-              14.15, ["get", "min_height"]
-            ],
-            'fill-extrusion-opacity': 0.6
-          }
-        })};
+          map.addSource("parking", {
+            type: "geojson",
+            data: parkingInfo as mapboxgl.GeoJSONSourceRaw["data"],
+          });
+          map.addLayer({
+            id: "parking-fill",
+            type: "fill",
+            source: "parking",
+            paint: {
+              "fill-color": "#c54848",
+              "fill-opacity": 0.5,
+            },
+          });
 
-      const selectedCalendar = JSON.parse(
-        localStorage.getItem("selectedCalendar") || "{}"
-      );
-
-      if (!selectedCalendar || !Array.isArray(selectedCalendar.combination)) return;
-
-      const { combination } = selectedCalendar;
-
-      const coords: coordsProps[] = [];
-
-      // Filter and create markers based on the selected day
-      combination.forEach((section: any) => {
-        section.meetTimes.forEach((meet: any) => {
-          if (meet.meetDays.includes(selectedDay)) {
-            const buildingCode = meet.meetBldgCode;
-            const { Longitude, Latitude } =
-              buildingCoords.features[buildingCode].properties;
-            coords.push({
-              name: "P" + meet.meetPeriodBegin + " - " + section.courseCode,
-              location: { longitude: Longitude, latitude: Latitude },
-            });
-          }
-        });
-      });
-
-      // Merge coordinates with identical locations
-      const mergedCoords = coords.reduce((acc: coordsProps[], current) => {
-        const found = acc.find(
-          (item) =>
-            item.location.longitude === current.location.longitude &&
-            item.location.latitude === current.location.latitude
-        );
-        if (found) {
-          found.name += `\n${current.name}`;
-        } else {
-          acc.push(current);
+          map.addLayer({
+            id: "add-3d-buildings",
+            source: "composite",
+            "source-layer": "building",
+            filter: ["==", "extrude", "true"],
+            type: "fill-extrusion",
+            minzoom: 14.1,
+            paint: {
+              "fill-extrusion-color": "#aaa",
+              "fill-extrusion-height": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                14.1,
+                0,
+                14.15,
+                ["get", "height"],
+              ],
+              "fill-extrusion-base": [
+                "interpolate",
+                ["linear"],
+                ["zoom"],
+                14.1,
+                0,
+                14.15,
+                ["get", "min_height"],
+              ],
+              "fill-extrusion-opacity": 0.6,
+            },
+          });
         }
-        return acc;
-      }, []);
 
-      mergedCoords.forEach((coord) => {
-        const el = document.createElement('div');
-        el.className = 'marker';
-        el.innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-pin" class="svg-inline--fa fa-map-pin pin-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#c54848" d="M320 144c0 79.5-64.5 144-144 144S32 223.5 32 144S96.5 0 176 0s144 64.5 144 144zM176 80c8.8 0 16-7.2 16-16s-7.2-16-16-16c-53 0-96 43-96 96c0 8.8 7.2 16 16 16s16-7.2 16-16c0-35.3 28.7-64 64-64zM144 480V317.1c10.4 1.9 21.1 2.9 32 2.9s21.6-1 32-2.9V480c0 17.7-14.3 32-32 32s-32-14.3-32-32z"></path></svg>';
-        el.style.width = '20px';
-        el.style.height = '20px';
+        const selectedCalendar = JSON.parse(
+          localStorage.getItem("selectedCalendar") || "{}"
+        );
 
-        const popup = new mapboxgl.Popup({
-          closeButton: false,
-          closeOnClick: false,
-          offset: 25,
-          className: "custom-popup",
-        })
-          .setText(coord.name)
-          .setHTML(coord.name.replace(/\n/g, "<br/>")); // Replace newline characters with HTML line breaks
+        if (!selectedCalendar || !Array.isArray(selectedCalendar.combination))
+          return;
 
-        new mapboxgl.Marker(el)
-          .setLngLat([coord.location.longitude, coord.location.latitude])
-          .setPopup(popup)
-          .addTo(map!);
+        const { combination } = selectedCalendar;
 
-        if (map)
-          popup
+        const coords: coordsProps[] = [];
+
+        // Filter and create markers based on the selected day
+        combination.forEach((section: any) => {
+          section.meetTimes.forEach((meet: any) => {
+            if (meet.meetDays.includes(selectedDay)) {
+              const buildingCode = meet.meetBldgCode;
+              const { Longitude, Latitude } =
+                buildingCoords.features[buildingCode].properties;
+              coords.push({
+                name: "P" + meet.meetPeriodBegin + " - " + section.courseCode,
+                location: { longitude: Longitude, latitude: Latitude },
+              });
+            }
+          });
+        });
+
+        // Merge coordinates with identical locations
+        const mergedCoords = coords.reduce((acc: coordsProps[], current) => {
+          const found = acc.find(
+            (item) =>
+              item.location.longitude === current.location.longitude &&
+              item.location.latitude === current.location.latitude
+          );
+          if (found) {
+            found.name += `\n${current.name}`;
+          } else {
+            acc.push(current);
+          }
+          return acc;
+        }, []);
+
+        mergedCoords.forEach((coord) => {
+          const el = document.createElement("div");
+          el.className = "marker";
+          el.innerHTML =
+            '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-pin" class="svg-inline--fa fa-map-pin pin-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#c54848" d="M320 144c0 79.5-64.5 144-144 144S32 223.5 32 144S96.5 0 176 0s144 64.5 144 144zM176 80c8.8 0 16-7.2 16-16s-7.2-16-16-16c-53 0-96 43-96 96c0 8.8 7.2 16 16 16s16-7.2 16-16c0-35.3 28.7-64 64-64zM144 480V317.1c10.4 1.9 21.1 2.9 32 2.9s21.6-1 32-2.9V480c0 17.7-14.3 32-32 32s-32-14.3-32-32z"></path></svg>';
+          el.style.width = "20px";
+          el.style.height = "20px";
+
+          const popup = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false,
+            offset: 25,
+            className: "custom-popup",
+          })
+            .setText(coord.name)
+            .setHTML(coord.name.replace(/\n/g, "<br/>")); // Replace newline characters with HTML line breaks
+
+          new mapboxgl.Marker(el)
             .setLngLat([coord.location.longitude, coord.location.latitude])
-            .addTo(map);
-      });
+            .setPopup(popup)
+            .addTo(map!);
 
-      // Add navigation control (the +/- zoom buttons)
-      if (map) map.addControl(new mapboxgl.NavigationControl(), "top-right");
-    });
-  }
+          if (map)
+            popup
+              .setLngLat([coord.location.longitude, coord.location.latitude])
+              .addTo(map);
+        });
+
+        // Add navigation control (the +/- zoom buttons)
+        if (map) map.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+        if (map){
+          map.on('click', 'parking-fill', function (e) {
+            if (e.features && e.features.length > 0) {
+              const feature = e.features[0];
+              // Ensure the geometry is a Polygon for accessing coordinates
+              if (feature.geometry.type === "Polygon") {
+                const description = feature.properties!.CUSTOM_POPUP;
+      
+                new mapboxgl.Popup()
+                  .setLngLat([e.lngLat.lng, e.lngLat.lat])
+                  .setHTML(description)
+                  .addTo(map!);
+              }
+            }
+          });
+      
+        // Change the cursor to a pointer when the mouse is over the parking-fill layer.
+        map.on('mouseenter', 'parking-fill', function () {
+            map!.getCanvas().style.cursor = 'pointer';
+        });
+      
+        // Change it back to a pointer when it leaves.
+        map.on('mouseleave', 'parking-fill', function () {
+            map!.getCanvas().style.cursor = '';
+        });
+      }
+      });
+    }
 
     // Clean up on unmount
     return () => {
