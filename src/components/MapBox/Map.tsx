e563102ef1c5aef6f825bb32bf25dbc0cd6566df
andychen482 <andy.chen0607@gmail.com>
2024-06-02 17:09:02 -0400

    Map pins now match color, less opaque isochrones

--- src/components/MapBox/Map.tsx
+++ src/components/MapBox/Map.tsx
@@ -84,7 +84,7 @@ const Map = () => {
         layout: {},
         paint: {
           "fill-color": color,
-          "fill-opacity": 0.5,
+          "fill-opacity": 0.05,
         },
       });
 
@@ -257,7 +257,7 @@ const Map = () => {
           const el = document.createElement("div");
           el.className = "marker";
           el.innerHTML =
-            '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-pin" class="svg-inline--fa fa-map-pin pin-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#c54848" d="M320 144c0 79.5-64.5 144-144 144S32 223.5 32 144S96.5 0 176 0s144 64.5 144 144zM176 80c8.8 0 16-7.2 16-16s-7.2-16-16-16c-53 0-96 43-96 96c0 8.8 7.2 16 16 16s16-7.2 16-16c0-35.3 28.7-64 64-64zM144 480V317.1c10.4 1.9 21.1 2.9 32 2.9s21.6-1 32-2.9V480c0 17.7-14.3 32-32 32s-32-14.3-32-32z"></path></svg>';
+            `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map-pin" class="svg-inline--fa fa-map-pin pin-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="${coord.color}" stroke="white" stroke-width="8px" d="M320 144c0 79.5-64.5 144-144 144S32 223.5 32 144S96.5 0 176 0s144 64.5 144 144zM176 80c8.8 0 16-7.2 16-16s-7.2-16-16-16c-53 0-96 43-96 96c0 8.8 7.2 16 16 16s16-7.2 16-16c0-35.3 28.7-64 64-64zM144 480V317.1c10.4 1.9 21.1 2.9 32 2.9s21.6-1 32-2.9V480c0 17.7-14.3 32-32 32s-32-14.3-32-32z"></path></svg>`;
           el.style.width = "20px";
           el.style.height = "20px";
 
